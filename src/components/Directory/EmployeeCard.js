2f1b80216a3baa5a67991ca2ae81b96c07b7c06f
Calvin <calvinnoestr@gmail.com>
2021-03-13 12:37:11 -0600

    updates

---
+++ src/components/Directory/EmployeeCard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Card, Row, Col, Image } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Directory.css';
+
+export default function EmployeeCard(props) {
+	return (
+		<>
+			{props.filteredResults.map((result) => (
+				<Card className="Employee-Card" key={result.login.uuid}>
+					<Row>
+						<Col className="IMG-Card-Col">
+							<Image src={result.picture.large} roundedCircle variant="top" className="IMG-Card" />
+						</Col>
+					</Row>
+					<Card.Body>
+						<Card.Title>{result.name.first + ' ' + result.name.last}</Card.Title>
+						<Card.Text className="Card-Text">
+							<b>Age: </b>
+							{result.dob.age}
+						</Card.Text>
+						<Card.Text className="Card-Text">
+							<b>Country: </b>
+							{result.location.country}
+						</Card.Text>
+						<Card.Text className="Card-Text">
+							<b>City: </b>
+							{result.location.city}
+						</Card.Text>
+						<Card.Text className="Card-Text">
+							<b>Phone: </b>
+							{result.phone}
+						</Card.Text>
+						<Card.Text className="Card-Text">
+							<b>Email: </b>
+							{result.email}
+						</Card.Text>
+					</Card.Body>
+					<Card.Footer>
+						
+					</Card.Footer>
+				</Card>
+			))}
+		</>
+	);
+}
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Navbar} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function NavbarBTS() {
+	return (
+		<>
+			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+				
+			</Navbar>
+		</>
+	);
+}
+
+export default NavbarBTS;
